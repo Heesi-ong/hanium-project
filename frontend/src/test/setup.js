6fc70439c32045caea1782d612030b233d9dcdc0
DoSangU <ehtkddn123@gmail.com>
2026-07-15 08:51:05 +0900

    style: 랜딩 페이지를 PresentAI 다크 테마로 전면 재구축

--- frontend/src/test/setup.js
+++ frontend/src/test/setup.js
@@ -2,6 +2,33 @@ import "@testing-library/jest-dom/vitest";
 import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 
+if (!window.matchMedia) {
+    window.matchMedia = (query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: () => {},
+        removeListener: () => {},
+        addEventListener: () => {},
+        removeEventListener: () => {},
+        dispatchEvent: () => false,
+    });
+}
+
+if (!window.IntersectionObserver) {
+    class MockIntersectionObserver {
+        observe() {}
+        unobserve() {}
+        disconnect() {}
+        takeRecords() {
+            return [];
+        }
+    }
+
+    window.IntersectionObserver = MockIntersectionObserver;
+    globalThis.IntersectionObserver = MockIntersectionObserver;
+}
+
 afterEach(() => {
     cleanup();
 });

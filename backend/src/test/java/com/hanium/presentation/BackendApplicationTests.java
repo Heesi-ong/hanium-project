c29c5cfe894dbc7efb0a568f13764f4597db447c
DoSangU <ehtkddn123@gmail.com>
2026-07-31 09:34:37 +0900

    설정: JPA OSIV 비활성화

--- backend/src/test/java/com/hanium/presentation/BackendApplicationTests.java
+++ backend/src/test/java/com/hanium/presentation/BackendApplicationTests.java
@@ -1,13 +1,26 @@
 package com.hanium.presentation;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+    @Autowired
+    private Environment environment;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void disablesOpenEntityManagerInView() {
+        assertThat(environment.getProperty("spring.jpa.open-in-view", Boolean.class))
+                .isFalse();
     }
 
 }
